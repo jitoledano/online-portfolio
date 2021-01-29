6ad18cfabb27a51bd11df5bc62e98157a05c2741
jitoledano <jitoledano98@gmail.com>
2021-01-29 12:52:43 -0700

    fixing typo in About me section

--- src/components/About/Data.js
+++ src/components/About/Data.js
@@ -7,7 +7,7 @@ export const homeObjOne = {
     lightTextDesc: true,
     topLine: 'About Me',
     headLine: 'My name is Jorge Isaac',
-    description: 'I initially starting coding in one of my college classes while I was at CSU, I quickly realized how much I love doing it. Since then, I have taken the initiative to learn as much as I possibly could. Outside of school, I have taught myself Python, HTML, CSS, React, and Javascript. While I continue to learn as much as possible, I have taken more time focusing on expanding my knowledge of React. If you would like to know more, feel free to contact me',
+    description: 'I initially started coding in one of my college classes while I was at CSU, I quickly realized how much I love doing it. Since then, I have taken the initiative to learn as much as I possibly could. Outside of school, I have taught myself Python, HTML, CSS, React, and Javascript. While I continue to learn as much as possible, I have taken more time focusing on expanding my knowledge of React. If you would like to know more, feel free to contact me',
     buttonLable: 'Contact me',
     imgStart: false,
     img: bgImg,
