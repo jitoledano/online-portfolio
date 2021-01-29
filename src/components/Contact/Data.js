fc47f09e92d1d10a26a1bc3943a9be23e7e2150e
jitoledano <jitoledano98@gmail.com>
2021-01-28 17:05:06 -0700

    Fixing typo in Contact page

--- src/components/Contact/Data.js
+++ src/components/Contact/Data.js
@@ -5,7 +5,7 @@ export const contactObj = {
     lightTextDesc: true,
     topLine: 'Contact',
     headLine: 'Want to work with me?',
-    description: 'If you would like to work with me, fill out the form provided and I will respond as soon as possible. You can also Call or Text me at 720-254-0264. I look forward to working witb you!',
+    description: 'If you would like to work with me, fill out the form provided and I will respond as soon as possible. You can also Call or Text me at 720-254-0264. I look forward to working with you!',
     dark: true,
     primary: true,
     darkText: false
